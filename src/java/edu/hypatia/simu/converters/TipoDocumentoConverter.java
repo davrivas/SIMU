f75ef87d1efb996a62236e51177d4572c10bbfd9
APRENDIZ <APRENDIZ@BOGDFPRDC8F863.FormacionSena.RED>
2018-06-19 17:44:51 -0500

    Modal de registro

---
+++ src/java/edu/hypatia/simu/converters/TipoDocumentoConverter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hypatia.simu.converters;
+
+import edu.hypatia.simu.modelo.dao.PersonaFacadeLocal;
+import edu.hypatia.simu.modelo.dao.TipoDocumentoFacadeLocal;
+import edu.hypatia.simu.modelo.entidades.Persona;
+import edu.hypatia.simu.modelo.entidades.TipoDocumento;
+import javax.enterprise.inject.spi.CDI;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+@FacesConverter(forClass = TipoDocumento.class)
+public class TipoDocumentoConverter implements Converter<TipoDocumento> {
+
+    private TipoDocumentoFacadeLocal tdfl;
+
+    public TipoDocumentoConverter() {
+        tdfl = CDI.current().select(TipoDocumentoFacadeLocal.class).get();
+    }
+
+    @Override
+    public TipoDocumento getAsObject(FacesContext context, UIComponent component, String value) {
+        try {
+            Integer id = Integer.valueOf(value);
+            return tdfl.find(id);
+        } catch (NumberFormatException numberFormatException) {
+            return null;
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext arg0, UIComponent arg1, TipoDocumento obj) {
+        if (obj != null) {
+            return obj.getIdTipoDocumento().toString();
+        }
+        return "";
+    }
+
+}
