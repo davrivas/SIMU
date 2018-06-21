f14536efa14459f9d3b45e81481c17855c8ef79d
NICOLAS <NICOLAS@FAMILIA_MUHU-PC>
2018-06-21 09:30:39 -0500

    editar perfil administrador, PDF de motos

---
+++ src/java/edu/hypatia/simu/controlador/export/ExportControlador.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hypatia.simu.controlador.export;
+
+import com.mysql.jdbc.Connection;
+import edu.hypatia.simu.modelo.dao.MotoFacadeLocal;
+import edu.hypatia.simu.modelo.entidades.Moto;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+@Named(value = "exportControlador")
+@SessionScoped
+public class ExportControlador implements Serializable {
+    
+    @EJB
+    private MotoFacadeLocal mfl;
+    
+    private Moto moto = new Moto();
+
+ 
+    public ExportControlador() {
+    }
+
+    public Moto getMoto() {
+        return moto;
+    }
+
+    public void setMoto(Moto moto) {
+        this.moto = moto;
+    }
+
+
+    
+    public void export() throws Exception{
+        
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_simu", "root", "");
+        
+        try {
+            FacesContext fc = FacesContext.getCurrentInstance();
+            ExternalContext ec = fc.getExternalContext();
+            File jasper = new File(ec.getRealPath("/WEB-INF/classes/reportes/motos.jasper"));
+            Map<String, Object> params = new HashMap<>();
+            params.put("logo", ec.getRealPath("/resources/img/logo-simu-negativo.jpg"));
+            JasperPrint jp = JasperFillManager.fillReport(jasper.getPath(), params, conexion);
+            HttpServletResponse hsr = (HttpServletResponse) ec.getResponse();
+            hsr.addHeader("Content-disposition", "attachment; filename=reporte.pdf");
+            OutputStream os = hsr.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(jp, os);
+            os.flush();
+            os.close();
+            fc.responseComplete();
+        } catch (IOException | JRException e) {
+        }
+    
+    }
+    
+}
